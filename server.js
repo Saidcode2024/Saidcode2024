91498926480b1471e7f69b444a4dbc7c3c18120a
Saidcode2024 <appcode2024@gmail.com>
2024-08-01 23:52:58 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+var server_filter = [
+    /*n0*/ 'كتب تهمك',
+    /*n1*/ 'التنمية البشرية وتطوير الذات',
+    /*n2*/ 'علم النفس',
+    /*n3*/ 'الشعر',
+    /*n4*/ 'التاريخ والحضارات',
+    /*n5*/ 'العلوم الاسلامية',
+]
+var server_books = {}
+var img_load = "https://drive.google.com/thumbnail?id="
+// n0
+server_books[0] = [
+    /*n1*/ [
+        /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67&sz=w500-h735' ,
+        /*name*/ 'كتاب كيف تؤثر على الآخرين وتكتسب الأصدقاء',
+        /*have*/ 'ديل كارنيجي',
+        /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+    ] ,
+    /*n2*/ [
+        /*img*/  img_load + '1DxpifjKUCOKwrML2sFRg75JNSYdKyAbK&sz=w500-h735' ,
+        /*name*/ 'كتاب شجاعة أن تكون غير محبوب',
+        /*have*/ 'مجموعة مؤلفين',
+        /*page*/ 'html/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
+    ] ,
+    /*n3*/ [
+        /*img*/  img_load + '1G2rvPzXlfRj6CHKK_sSgezpsP3PuWMpW&sz=w452-h678' ,
+        /*name*/ 'كتاب خذها بقوة',
+        /*have*/ 'حسام عبد العزيز',
+        /*page*/ 'html/كتاب_خذها_بقوة.html'
+    ] ,
+]
+// n1
+server_books[1] = [
+    /*n1*/ [
+        /*img*/  img_load + '1C-D2dApVTTfBrY3D2tQf7Esq7xAlsT67&sz=w500-h735' ,
+        /*name*/ 'كتاب كيف تؤثر على الآخرين وتكتسب الأصدقاء',
+        /*have*/ 'ديل كارنيجي',
+        /*page*/ 'html/كتاب_كيف_تؤثر_على_الآخرين_وتكتسب_الأصدقاء.html'
+    ] ,
+    /*n2*/ [
+        /*img*/  img_load + '1DxpifjKUCOKwrML2sFRg75JNSYdKyAbK&sz=w500-h735' ,
+        /*name*/ 'كتاب شجاعة أن تكون غير محبوب',
+        /*have*/ 'مجموعة مؤلفين',
+        /*page*/ 'html/كتاب_شجاعة_أن_تكون_غير_محبوب.html'
+    ] ,
+    /*n3*/ [
+        /*img*/  img_load + '1G2rvPzXlfRj6CHKK_sSgezpsP3PuWMpW&sz=w452-h678' ,
+        /*name*/ 'كتاب خذها بقوة',
+        /*have*/ 'حسام عبد العزيز',
+        /*page*/ 'html/كتاب_خذها_بقوة.html'
+    ] ,
+]
+// n2
+server_books[2] = [
+
+]
+// n3
+server_books[3] = [
+
+]
+// n4
+server_books[4] = [
+
+]
+// n5
+server_books[5] = [
+
+]
